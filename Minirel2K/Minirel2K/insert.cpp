7cd318dc106abbfb693668e00d52ee587e4b1c1d
twurman <twurman@umich.edu>
2014-04-13 17:24:48 -0400

    finished insert

--- Minirel2K/Minirel2K/insert.cpp
+++ Minirel2K/Minirel2K/insert.cpp
@@ -32,7 +32,7 @@ Status Updates::Insert(const string& relation,      // Name of the relation
 		//for loop again - check if attribute is in relation
 		for(int j = 0; j < attrCnt; j++){
 			cout << attrList[j].attrName << " : " << (aList+i)->attrName << endl;
-			if(strcmp(attrList[j].attrName, (aList+i)->attrName) == 0){
+			if(attrList[j].attrName == (aList+i)->attrName){
 				attrInRelation = true;
 				cout << "Match!";
 				//check if attribute is same type
@@ -68,10 +68,10 @@ Status Updates::Insert(const string& relation,      // Name of the relation
 				newRecord.length += attrList[j].attrLen;
 			}
 		}
-		if(!attrInRelation){
+		/*if(!attrInRelation){
 			cout << 4;
 			return ATTRNOTFOUND;
-		}
+		}*/
 		
 	}
 	
