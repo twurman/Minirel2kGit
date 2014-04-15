58ee7b421a3fc295f58e4c115af9461043c6c66f
twurman <twurman@umich.edu>
2014-04-15 01:19:04 -0400

    testing select parser

--- Minirel2K/Minirel2K/indexselect.cpp
+++ Minirel2K/Minirel2K/indexselect.cpp
@@ -1,6 +1,10 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include "heapfile.h"
+#include <string.h>
+#include <stdlib.h>
+
 Status Operators::IndexSelect(const string& result,       // Name of the output relation
                               const int projCnt,          // Number of attributes in the projection
                               const AttrDesc projNames[], // Projection list (as AttrDesc)
