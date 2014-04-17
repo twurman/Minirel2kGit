6f7acf8d13dc3f37bf6416ae77e361d2b0013637
twurman <twurman@umich.edu>
2014-04-17 18:47:54 -0400

    adding new test cases

--- Minirel2K/Minirel2K/smj.cpp
+++ Minirel2K/Minirel2K/smj.cpp
@@ -56,6 +56,218 @@ Status Operators::SMJ(const string& result,           // Output relation name
 		return getRes;
 	}
 	
+	/* Merge */
+	Record leftRec, rightRec, newRec;
+	newRec.data = malloc(reclen);
+	newRec.length = 0;
+	RID leftRid, rightRid, newRecRID;
+	Status leftIsGood = left.next(leftRec);
+	if(leftIsGood != OK){
+		free(newRec.data);
+		return leftIsGood;
+	}
+	Status rightIsGood = right.next(rightRec);
+	if(rightIsGood != OK){
+		free(newRec.data);
+		return rightIsGood;
+	}
+	
+	
+	
+	while(rightIsGood == OK && leftIsGood == OK){
+		
+		int compare = matchRec(leftRec, rightRec, attrDesc1, attrDesc2);
+		if (compare > 0){ //right key < left key
+			rightIsGood = right.next(rightRec);
+		}
+		else if (compare < 0){ //left key < right key
+			leftIsGood = left.next(leftRec);
+		}
+		else { // left key == right key
+			
+			//put left U right projected into result record
+			newRec.length = 0;
+			for (int i = 0; i < projCnt; i++){
+				if (attrDescArray[i].relName == attrDesc1.relName){
+					memcpy((char*)newRec.data + newRec.length, (char*)leftRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+				}
+				else {
+					memcpy((char*)newRec.data + newRec.length, (char*)rightRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+				}
+				newRec.length += attrDescArray[i].attrLen;
+			}
+			//insert projected record into result
+			res.insertRecord(newRec, newRecRID);
+			
+			
+			/* output further tuples that match w/ right */
+			Status rMark = right.setMark();
+			Status lMark = left.setMark();
+			Record leftRec2, rightRec2;
+			Status left2_good, right2_good;
+			
+			left2_good = left.next(leftRec2);
+			while (left2_good == OK && matchRec(leftRec2, rightRec, attrDesc1, attrDesc2) == 0){
+				
+				/* left2 U right projected into result record */
+				newRec.length = 0;
+				for (int i = 0; i < projCnt; i++){
+					if (attrDescArray[i].relName == attrDesc1.relName){
+						memcpy((char*)newRec.data + newRec.length, (char*)leftRec2.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+					}
+					else {
+						memcpy((char*)newRec.data + newRec.length, (char*)rightRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+					}
+					newRec.length += attrDescArray[i].attrLen;
+				}
+				//insert projected record into result
+				res.insertRecord(newRec, newRecRID);
+				
+				left2_good = left.next(leftRec2);
+			}
+			/* output further tuples that match with q */
+			while (right2_good == OK && matchRec(leftRec, rightRec2, attrDesc1, attrDesc2) == 0){
+				/* left U right2 projected into result record */
+				newRec.length = 0;
+				for (int i = 0; i < projCnt; i++){
+					if (attrDescArray[i].relName == attrDesc1.relName){
+						memcpy((char*)newRec.data + newRec.length, (char*)leftRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+					}
+					else {
+						memcpy((char*)newRec.data + newRec.length, (char*)rightRec2.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+					}
+					newRec.length += attrDescArray[i].attrLen;
+				}
+				//insert projected record into result
+				res.insertRecord(newRec, newRecRID);
+				
+				right2_good = right.next(rightRec2);
+			}
+			
+			//return to previous spots
+			rightIsGood = right.gotoMark();
+			leftIsGood = left.gotoMark();
+			if (rightIsGood != OK || leftIsGood != OK){
+				if (rightIsGood != OK)
+					return rightIsGood;
+				else
+					return leftIsGood;
+			}
+			rightIsGood = right.next(rightRec);
+			leftIsGood = left.next(leftRec);
+		}
+	}
+	
+	/*
+	 if(compare == 0){
+	 Status setmark = right.setMark();
+	 if(setmark != OK){
+	 free(newRec.data);
+	 return setmark;
+	 }
+	 }
+	 while(compare == 0){
+	 newRec.length = 0;
+	 //project the joined tuple
+	 for (int i = 0; i < projCnt; i++){
+	 if (attrDescArray[i].relName == attrDesc1.relName){
+	 memcpy((char*)newRec.data + newRec.length, (char*)leftRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+	 }
+	 else {
+	 memcpy((char*)newRec.data + newRec.length, (char*)rightRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
+	 }
+	 newRec.length += attrDescArray[i].attrLen;
+	 }
+	 //insert projected record into result
+	 res.insertRecord(newRec, newRecRID);
+	 
+	 rightIsGood = right.next(rightRec);
+	 if (rightIsGood == OK){
+	 compare = matchRec(leftRec, rightRec, attrDesc1, attrDesc2);
+	 }
+	 else {
+	 compare = -1; //break out of while loop
+	 }
+	 
+	 
+	 }
+	 
+	 if (compare < 0){
+	 //scan the next on the left
+	 Record nextRec;
+	 leftIsGood = left.next(nextRec);
+	 if(leftIsGood == OK){	//continue, otherwise ENDOFPAGE and done
+	 if(matchRec(leftRec, nextRec, attrDesc1, attrDesc1) == 0){
+	 rightIsGood = right.gotoMark();
+	 if(rightIsGood != OK){
+	 free(newRec.data);
+	 return rightIsGood;
+	 }
+	 }
+	 leftRec.data = nextRec.data;
+	 leftRec.length = nextRec.length;
+	 }
+	 
+	 } else {
+	 //right is smaller, scan the next right tuple and set the mark
+	 rightIsGood = right.next(rightRec);
+	 Status setmark = right.setMark();
+	 if(setmark != OK){
+	 free(newRec.data);
+	 return setmark;
+	 }
+	 }
+	 
+	 
+	 } */
+	
+	
+	
+	
+	
+	free(newRec.data);
+	return OK;
+	
+	
+	
+	/*Status getRes = OK;
+	HeapFile res = HeapFile(result, getRes);
+	if(getRes != OK){
+		return getRes;
+	}
+	
+	//sort the left relation
+	//calculate the number of unpinned pages to use
+	int k = .8 * (bufMgr->numUnpinnedPages());
+	
+	//get the description of the relation & add up the size of all the attributes
+	AttrDesc *relationStats;
+	int numAttr, recSize = 0;
+	attrCat->getRelInfo(attrDesc1.relName, numAttr, relationStats);
+	for(int i = 0; i < numAttr; i++){
+		recSize += relationStats[i].attrLen;
+	}
+	
+	//calculation the number of tuples (aka max items) with k pages available
+	int numTuples = k * PAGESIZE / recSize;
+	SortedFile left = SortedFile(attrDesc1.relName, attrDesc1.attrOffset, attrDesc1.attrLen, (Datatype)attrDesc1.attrType, numTuples, getRes);
+	if (getRes != OK){
+		return getRes;
+	}
+	
+	//now do same calculation to sort the right relation
+	k = .8 * (bufMgr->numUnpinnedPages());
+	recSize = 0;
+	attrCat->getRelInfo(attrDesc2.relName, numAttr, relationStats);
+	for(int i = 0; i < numAttr; i++){
+		recSize += relationStats[i].attrLen;
+	}
+	numTuples = k * PAGESIZE / recSize;
+	SortedFile right = SortedFile(attrDesc2.relName, attrDesc2.attrOffset, attrDesc2.attrLen, (Datatype)attrDesc2.attrType, numTuples, getRes);
+	if (getRes != OK){
+		return getRes;
+	}
+	
 
 	Record leftRec, rightRec, newRec;
 	newRec.data = malloc(reclen);
@@ -116,7 +328,7 @@ Status Operators::SMJ(const string& result,           // Output relation name
 		}
 		
 		
-	}
+	}*/
 	
 	/*
 	
