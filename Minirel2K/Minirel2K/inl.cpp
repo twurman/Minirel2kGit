04278d3357068953e3167373c32968a05edab2da
twurman <twurman@umich.edu>
2014-04-17 13:39:50 -0400

    updated join

--- Minirel2K/Minirel2K/inl.cpp
+++ Minirel2K/Minirel2K/inl.cpp
@@ -112,14 +112,10 @@ Status Operators::INL(const string& result,           // Name of the output rela
 			
 			if(compare == 0){
 				for (int i = 0; i < projCnt; i++){
-					cerr << attrDescArray[i].relName << endl;
-					cerr << attrDesc1.relName << " : " << attrDesc2.relName << endl;
-					if (attrDescArray[i].relName == attrDesc1.relName){
-						cerr << "Get attribute from 1" << endl;
+					if (strcmp(attrDescArray[i].relName, attrDesc1.relName) == 0){
 						memcpy((char*)newRec.data + newRec.length, (char*)outerRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
 					}
 					else {
-						cerr << "Get attribute from 2" << endl;
 						memcpy((char*)newRec.data + newRec.length, (char*)innerRec.data+attrDescArray[i].attrOffset, attrDescArray[i].attrLen);
 					}
 					newRec.length += attrDescArray[i].attrLen;
