570dea3f8301f2e26f5704655b768cb73128b40b
twurman <twurman@umich.edu>
2014-04-18 15:41:50 -0400

    adding new test cases

--- Minirel2K/Minirel2K/sql/test34.sql
+++ Minirel2K/Minirel2K/sql/test34.sql
@@ -2,44 +2,107 @@
 CREATE TABLE gdis(gid integer, name char(34), occupation char(34));
 CREATE TABLE doggs(did integer, real_name char(34), occupation char(34));
 
-insert into gdis(gid, name, occupation)
-values (1, 'ryan wolande', 'pledge master');
-insert into gdis(gid, name, occupation)
-values(2, 'anders ders', 'fisherman');
-insert into gdis(gid, name, occupation)
-values(3, 'dersly', 'blue heron');
-insert into gdis(gid, name, occupation)
-values(4, 'chis', 'cherbs');
+-- insert into gdis
+INSERT INTO gdis(gid, name, occupation)
+	VALUES (1, 'ryan wolande', 'pledge master');
+INSERT INTO gdis(gid, name, occupation)
+	VALUES(2, 'anders ders', 'fisherman');
+INSERT INTO gdis(gid, name, occupation)
+	VALUES(3, 'dersly', 'blue heron');
+INSERT INTO gdis(gid, name, occupation)
+	VALUES(4, 'chis', 'cherbs');
 
-insert into doggs(did, real_name, occupation)
-values (1,'timmy wurm', 'cherbs');
-insert into doggs(did, real_name, occupation)
-values(2, 'the realest SC', 'fisherman');
-insert into doggs(did, real_name, occupation)
-values(6, 'gert dogg', 'fisherman');
-insert into doggs(did, real_name, occupation)
-values(7, 'nate dogg', 'CEO');
+-- insert into doggs
+INSERT INTO doggs(did, real_name, occupation)
+	VALUES (3,'timmy wurm', 'cherbs');
+INSERT INTO doggs(did, real_name, occupation)
+	VALUES(4, 'the realest SC', 'fisherman');
+INSERT INTO doggs(did, real_name, occupation)
+	VALUES(6, 'gert dogg', 'fisherman');
+INSERT INTO doggs(did, real_name, occupation)
+	VALUES(7, 'nate dogg', 'CEO');
 
-select * from doggs;
-select gdis.name, gdis.occupation from gdis;
-select gid from gdis where occupation = 'blue heron';
-select * from doggs, gdis where doggs.occupation = gdis.occupation
+
+-- test select * with no where clause
+SELECT * FROM doggs;
+
+-- test select with projection and no where clause
+SELECT gdis.name, gdis.occupation FROM gdis;
+
+-- test select * with a where clause
+SELECT * FROM gdis WHERE gdis.gid > 2;
+
+-- test select with projection and where clause
+SELECT gdis.name, gdis.occupation FROM gdis WHERE gdis.occupation = 'blue heron';
+
+-- smj joins
+SELECT * FROM doggs, gdis WHERE doggs.occupation = gdis.occupation
+
+SELECT gdis.name, doggs.real_name, gdis.occupation, doggs.occupation FROM gdis, doggs WHERE gdis.gid = doggs.did;
+
+-- inl joins
+CREATE INDEX gdis(gid);
+CREATE INDEX doggs(did);
+
+SELECT * FROM doggs, gdis WHERE doggs.occupation = gdis.occupation
+
+SELECT gdis.name, doggs.real_name, gdis.occupation, doggs.occupation FROM gdis, doggs WHERE gdis.gid = doggs.did;
+
+-- index select
+SELECT gdis.name, gdis.occupation FROM gdis WHERE gdis.name = 'chis';
+SELECT * FROM doggs WHERE doggs.did = 3;
+SELECT * FROM gdis WHERE gdis.gid = 5;
+
+DROP INDEX gdis(gid);
+DROP INDEX doggs(did);
+
+-- snl joins
+SELECT gdis.name, doggs.real_name, gdis.occupation, doggs.occupation FROM gdis, doggs WHERE gdis.gid > doggs.did;
+
+-- should return 3 rows, 3&3, 3&4, and 4&4
+SELECT * FROM gdis, doggs WHERE gdis.gid <= doggs.did;
+
+-- should return 1 row, 3&4
+SELECT * FROM gdis, doggs WHERE gdis.gid < doggs.did;
+
+-- should return 1 row, 4&3
+SELECT * FROM gdis, doggs WHERE doggs.did > gdis.gid;
+
+-- should return 3 rows, 3&3, 4&3, 4&4
+SELECT * FROM gdis, doggs WHERE doggs.did >= gdis.gid;
+
+-- should be cross product minus when did == gid
+SELECT * FROM gdis, doggs WHERE doggs.did != gdis.gid;
+
+
+DROP TABLE gdis;
+DROP TABLE doggs;
 
 -------test 2---------
-create table redTeam(id integer, red integer);
-create table blueTeam(id integer, blue integer);
+CREATE TABLE redTeam(id integer, red double);
+CREATE TABLE blueTeam(id integer, blue double);
 
-insert into redTeam(id, red)
-values(2,4);
-insert into redTeam(id, red)
-values(2,5);
-insert into redTeam(id, red)
-values(3,5);
-insert into blueTeam(id, blue)
-values(1,3);
-insert into blueTeam(id, blue)
-values(1,4);
-insert into blueTeam(id, blue)
-values(3,3);
+INSERT INTO redTeam(id, red)
+	VALUES(2,4);
+INSERT INTO redTeam(id, red)
+	VALUES(2,5);
+INSERT INTO redTeam(id, red)
+	VALUES(3,5);
+INSERT INTO blueTeam(id, blue)
+	VALUES(1,3);
+INSERT INTO blueTeam(id, blue)
+	VALUES(1,4);
+INSERT INTO blueTeam(id, blue)
+	VALUES(3,3);
 
-select blue.blueTeam, red.blue team from blueTeam, redTeam where blueTeam.id = redTeam.id;
+SELECT blue.blueTeam, red.blue team from blueTeam, redTeam where blueTeam.id = redTeam.id;
+
+
+DROP TABLE redTeam;
+DROP TABLE blueTeam;
+
+
+
+
+
+
